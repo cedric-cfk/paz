aa50a8d2961b63150d20e440f5244f9d220f67af
kraenzle <kraenzle@uni-bremen.de>
2023-09-01 14:03:05 +0200

    Fixed and Improved Datagenerator

--- paz/datasets/vvad_lrs3.py
+++ paz/datasets/vvad_lrs3.py
@@ -48,57 +48,60 @@ class VVAD_LRS3(Generator):
             self.total_size = data.get('x_test').shape[0]
         data.close()
 
-    def __call__(self):
-        data = h5py.File(self.path, mode='r')
-
-        if not self.testing:
-            x_train = data.get("x_train")
-            y_train = data.get("y_train")
-        else:
-            x_train = data.get("x_test")
-            y_train = data.get("y_test")
-
         # NotTODO add the 200 test samples to those (if so add those 200 to the self.total_size). It is not worth it. it is roughly 0.5% of the dataset but would add aditional commands to the dataset generator for each iteration. which could increase the training time.
         indexes_pos = list(range(self.total_size // 2))
         indexes_neg = list(range(self.total_size // 2, self.total_size))
 
         random.Random(445363).shuffle(indexes_pos)
-        random.Random(445363).shuffle(indexes_neg)
+        random.Random(848641).shuffle(indexes_neg)
 
-        indexes_val = []
-        indexes_test = []
+        self.indexes_val = []
+        self.indexes_test = []
 
         old_pos_size = len(indexes_pos)
         old_neg_size = len(indexes_neg)
 
         # val split
         if self.val_split > 0.0:
-            indexes_val = indexes_pos[:int(self.val_split * old_pos_size)] + indexes_neg[:int(self.val_split * old_neg_size)]
-            indexes_pos = indexes_pos[int(self.val_split * old_pos_size):]
-            indexes_neg = indexes_neg[int(self.val_split * old_neg_size):]
+            val_split_size = int(self.val_split * 0.5 * self.total_size)
+            self.indexes_val = indexes_pos[:val_split_size] + indexes_neg[:val_split_size]
+            indexes_pos = indexes_pos[val_split_size:]
+            indexes_neg = indexes_neg[val_split_size:]
 
         # test split
         if self.test_split > 0.0:
-            indexes_test = indexes_pos[:int(self.test_split * old_pos_size)] + indexes_neg[:int(self.test_split * old_neg_size)]
-            indexes_pos = indexes_pos[int(self.test_split * old_pos_size):]
-            indexes_neg = indexes_neg[int(self.test_split * old_neg_size):]
+            test_split_size = int(self.test_split * 0.5 * self.total_size)
+            self.indexes_test = indexes_pos[:test_split_size] + indexes_neg[:test_split_size]
+            indexes_pos = indexes_pos[test_split_size:]
+            indexes_neg = indexes_neg[test_split_size:]
 
-        indexes_train = indexes_pos + indexes_neg
-        random.Random(445363).shuffle(indexes_train)  # Use always the same seed so every model gets the same shuffle
+        self.indexes_train = indexes_pos + indexes_neg
 
-        # print("indexes_val", len(indexes_val))
-        # print("indexes_test", len(indexes_test))
-        # print("indexes_train", len(indexes_train))
+        random.seed(445363)
 
+    def __call__(self):
+        print("indexes_val", len(self.indexes_val))
+        print("indexes_test", len(self.indexes_test))
+        print("indexes_train", len(self.indexes_train))
+        indexes = []
         if self.split == 'train':
-            indexes = indexes_train
+            indexes = self.indexes_train
+            random.shuffle(indexes)  # Use always the same seed so every model gets the same shuffle
         elif self.split == 'val':
-            indexes = indexes_val
+            indexes = self.indexes_val
         elif self.split == 'test':
-            indexes = indexes_test
+            indexes = self.indexes_test
 
         # print("Selected split: ", self.split)
         # print("indexes", len(indexes))
+
+        data = h5py.File(self.path, mode='r')
+        if not self.testing:
+            x_train = data.get("x_train")
+            y_train = data.get("y_train")
+        else:
+            x_train = data.get("x_test")
+            y_train = data.get("y_test")
 
         for i in indexes:
             yield (x_train[i], y_train[i])
@@ -109,8 +112,11 @@ class VVAD_LRS3(Generator):
             raise ValueError('You need to call __call__ first to set the total_size')
 
         if self.split == 'train':
-            return self.total_size - int(self.total_size * self.val_split) - int(self.total_size * self.test_split)
+            print("total_size_totoal", self.total_size - int(self.val_split * 0.5 * self.total_size) * 2 - int(self.test_split * 0.5 * self.total_size) * 2)
+            return self.total_size - int(self.val_split * 0.5 * self.total_size) * 2 - int(self.test_split * 0.5 * self.total_size) * 2
         elif self.split == 'val':
-            return int(self.total_size * self.val_split)
+            print("total_size_val", int(self.val_split * 0.5 * self.total_size) * 2)
+            return int(self.val_split * 0.5 * self.total_size) * 2
         elif self.split == 'test':
-            return int(self.total_size * self.test_split)
+            print("total_size_test", self.total_size)
+            return int(self.test_split * 0.5 * self.total_size) * 2
