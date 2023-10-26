c75c70036a1a12f6036d61e11ec12f66bb7bf119
kraenzle <kraenzle@uni-bremen.de>
2023-10-26 16:42:19 +0200

    Evaluation Pipeline finished ready for testing

--- examples/visual_voice_activity_detection/evaluation/eval1.py
+++ examples/visual_voice_activity_detection/evaluation/eval1.py
@@ -1,19 +1,15 @@
-# import os
 import argparse
-# import datetime
-# import math
-# import helper_functions
-# from codecarbon import OfflineEmissionsTracker
+import os.path
+
+import helper_functions
 from tensorflow.python.data import Dataset
 import tensorflow as tf
 
-# from paz.models.classification.cnn2Plus1 import CNN2Plus1D_Light, CNN2Plus1D_Filters, CNN2Plus1D_Layers
+from paz.datasets import VVAD_LRS3
+from paz.models.classification import CNN2Plus1D, VVAD_LRS3_LSTM, MoViNet, CNN2Plus1D_Light, CNN2Plus1D_Layers, CNN2Plus1D_Filters
 
 keras = tf.keras
 from keras.losses import BinaryCrossentropy
-
-from paz.datasets import VVAD_LRS3
-from paz.models.classification import CNN2Plus1D, VVAD_LRS3_LSTM, MoViNet, CNN2Plus1D_Light, CNN2Plus1D_Layers, CNN2Plus1D_Filters
 
 
 parser = argparse.ArgumentParser(description='Paz VVAD Training')
@@ -24,25 +20,27 @@ parser.add_argument('--weight_path', type=str,
                     default=None,
                     help='Path to model weights')
 parser.add_argument('-m', '--model', type=str,
-                    default='VVAD_LRS3',
+                    default='CNN2Plus1D',
                     help='Model you want to train',
                     choices=['VVAD_LRS3', 'CNN2Plus1D', 'CNN2Plus1DLight', 'CNN2Plus1DLayers', 'CNN2Plus1DFilters', 'MoViNets', 'ViViT'])
 parser.add_argument('-b', '--batch_size', type=int,
                     default=16,
                     help='Batch size for training and validation')
-parser.add_argument('-e', '--epochs', type=int,
-                    default=200,
-                    help='Epochs for training')
-parser.add_argument('-w', '--warmup', type=int,
-                    default=5,
-                    help='Warmup epochs for training')
 parser.add_argument('-o', '--output_path', type=str,
                     default="./output/",
                     help='Path to directory for saving outputs.')
 parser.add_argument('--testing', action='store_true', help='Use the test split instead of the validation split')
+parser.add_argument('--use_multiprocessing', action='store_true', help='Use multiprocessing for data loading')
+parser.add_argument('--workers', type=int, default=5, help='Number of workers for data loading')
+parser.add_argument('--max_queue_size', type=int, default=15, help='Max queue size for data loading')
 args = parser.parse_args()
 
-generatorVal = VVAD_LRS3(path=args.data_path, split="val", testing=args.testing, val_split=0.1, test_split=0.1)
+try:
+    os.mkdir(os.path.join(os.path.join(args.output_path, args.model)))
+except FileExistsError:
+    pass
+
+generatorVal = VVAD_LRS3(path=args.data_path, split="val", testing=args.testing, val_split=0.1, test_split=0.1, evaluating=True)
 
 datasetVal = Dataset.from_generator(generatorVal, output_signature=(tf.TensorSpec(shape=(38, 96, 96, 3)), tf.TensorSpec(shape=(), dtype=tf.int8)))
 
@@ -56,33 +54,42 @@ datasetVal = datasetVal.padded_batch(1)  # args.batch_size
 
 model = None
 if args.weight_path is None:
-    model = CNN2Plus1D(weights="yes", input_shape=(38, 96, 96, 3))
-    # model = CNN2Plus1D_Light(weights="yes", input_shape=(38, 96, 96, 3))
-    # model = CNN2Plus1D_Layers(weights="yes", input_shape=(38, 96, 96, 3))
-    # model = VVAD_LRS3_LSTM(weights="yes", input_shape=(38, 96, 96, 3))
-
+    if args.model == "VVAD_LRS3":
+        model = VVAD_LRS3_LSTM(weights="yes", input_shape=(38, 96, 96, 3))
+    elif args.model == "CNN2Plus1D":
+        model = CNN2Plus1D(weights="yes", input_shape=(38, 96, 96, 3))
+    elif args.model == "CNN2Plus1DLight":
+        model = CNN2Plus1D_Light(weights="yes", input_shape=(38, 96, 96, 3))
+    elif args.model == "CNN2Plus1DLayers":
+        model = CNN2Plus1D_Layers(weights="yes", input_shape=(38, 96, 96, 3))
+    elif args.model == "CNN2Plus1DFilters":
+        model = CNN2Plus1D_Filters(weights="yes", input_shape=(38, 96, 96, 3))
+    else:
+        raise NotImplemented("Not implemented yet")
 else:
-    model = CNN2Plus1D(weights="yes", input_shape=(38, 96, 96, 3), tmp_weights_path=args.weight_path)
-    # model = CNN2Plus1D_Light(weights="yes", input_shape=(38, 96, 96, 3), tmp_weights_path=args.weight_path)
-    # model = CNN2Plus1D_Layers(weights="yes", input_shape=(38, 96, 96, 3))
-    # model = VVAD_LRS3_LSTM(weights="yes", input_shape=(38, 96, 96, 3))
+    if args.model == "VVAD_LRS3":
+        model = VVAD_LRS3_LSTM(weights="yes", input_shape=(38, 96, 96, 3))
+    elif args.model == "CNN2Plus1D":
+        model = CNN2Plus1D(weights="yes", input_shape=(38, 96, 96, 3), tmp_weights_path=args.weight_path)
+    elif args.model == "CNN2Plus1DLight":
+        model = CNN2Plus1D_Light(weights="yes", input_shape=(38, 96, 96, 3), tmp_weights_path=args.weight_path)
+    elif args.model == "CNN2Plus1DLayers":
+        model = CNN2Plus1D_Layers(weights="yes", input_shape=(38, 96, 96, 3))
+    elif args.model == "CNN2Plus1DFilters":
+        model = CNN2Plus1D_Filters(weights="yes", input_shape=(38, 96, 96, 3))
+    else:
+        raise NotImplemented("Not implemented yet")
 
-model.summary()
+if not tf.config.list_physical_devices('GPU'):
+    print("No GPU was detected. No GPU memory usage will be logged.")
+model.predict(datasetVal,
+              callbacks=[helper_functions.CSVLoggerEval(args.output_path, args.model, data_generator=generatorVal)],
+              max_queue_size=args.max_queue_size,
+              workers=args.workers,
+              use_multiprocessing=args.use_multiprocessing)
 
-# TODO for memory usage:
-# https://www.tensorflow.org/api_docs/python/tf/config/experimental/get_memory_info
 
 from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2_as_graph
-
-if tf.config.list_physical_devices('GPU'):
-    print(tf.config.experimental.get_memory_info("GPU:0"))
-else:
-    print("No GPU found")
-model.predict(datasetVal)
-if tf.config.list_physical_devices('GPU'):
-    print(tf.config.experimental.get_memory_info("GPU:0"))
-
-
 def get_flops(model):
     concrete = tf.function(lambda inputs: model(inputs))
     concrete_func = concrete.get_concrete_function(
@@ -95,7 +102,6 @@ def get_flops(model):
         flops = tf.compat.v1.profiler.profile(graph=graph, run_meta=run_meta, cmd="op", options=opts)
         return flops.total_float_ops
 
-
-
 # model.summary()
+# keras.utils.plot_model(model, expand_nested=True, dpi=60, show_shapes=True)
 # print("The FLOPs is:{}".format(get_flops(model)) ,flush=True )
